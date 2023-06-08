22bd21274ab924c5be58b5b93556cdfba405d5e6
picoting <lindsayl.6857@gmail.com>
2023-06-08 11:53:48 -0400

    added address validation

--- directions.py
+++ directions.py
@@ -1,5 +1,8 @@
 import requests
 
+#api key
+api_key = 'AIzaSyD8hzf6RtCQ8ab6AYdt7M6J-Nr2tgvuz0M'
+
 def get_directions_duration(start, destination, mode, api_key):
     url = f"https://maps.googleapis.com/maps/api/directions/json?origin={start}&destination={destination}&mode={mode}&key={api_key}"
     response = requests.get(url)
@@ -23,17 +26,37 @@ def get_directions_duration(start, destination, mode, api_key):
     
     return None
 
-#api key
-api_key = ''
+def validate_address(address, api_key): #returns tuple of formatted address and zip code
+    url = f"https://maps.googleapis.com/maps/api/geocode/json?address={address}&key={api_key}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        if data["status"] == "OK":
+            #get formatted address
+            formatted_address = data["results"][0]["formatted_address"]
+            #take zipcode for address
+            postal_code = None
+            for component in data["results"][0]["address_components"]:
+                if "postal_code" in component["types"]:
+                    postal_code = component["long_name"]
+                    break
+            return formatted_address, postal_code
+        else:
+            print("Error: Address validation failed.")
+    else:
+        print("Error: Failed to validate address.")
+
+    return None, None
 
 #put in start and destination (city, state) format as of rn
-start = '575 Commonwealth Ave Boston, MA'
-destination = '775 Beacon St Boston, MA'
+start = ''
+destination = ''
 
 # not sure if this part is busted or the function
-modes = ['driving', 'walking', 'bicycling', 'walking']
+modes = ['driving', 'bicycling', 'transit', 'walking']
 for mode in modes:
     directions_durations = get_directions_duration(start, destination, mode, api_key)
     if directions_durations:
-        for mode, duration in directions_durations.items():
-            print(f"{mode.capitalize()}: {duration}")
+        for modes, duration in directions_durations.items(): #this displays "transit" as "walking" not sure why
+            print(f"{mode.capitalize()}: {duration}") #prints mode from the list and not the dictionary
