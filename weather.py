9687b6e3ea54e0f308042e9b897ce66196f46026
celinec1 <cchen@bu.edu>
2023-06-08 00:11:03 -0400

    small changes to vars

--- weather.py
+++ weather.py
@@ -3,7 +3,7 @@ import geolocate
 
 #pulls data from weatherapi, and using the geolocate call it is able to pull the data from the location the person is at
 api_key = ""
-w_api_key = ""
+g_api_key = ""
 
 def get_weather(latitude, longitude, api_key):
     url = f"https://api.weatherapi.com/v1/current.json?q={latitude},{longitude}&key={api_key}"
@@ -20,7 +20,7 @@ def get_weather(latitude, longitude, api_key):
         print(f"Condition: {current_weather['condition']['text']}")
 
 # retrieve latitude and longitude from geolocate module
-location = geolocate.get_current_location(w_api_key)
+location = geolocate.get_current_location(g_api_key)
 if location:
     latitude, longitude = location
     get_weather(latitude, longitude, api_key)
