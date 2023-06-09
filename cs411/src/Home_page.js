ff414fcb28f7b849688799176eabf76b7e622bb9
celinec1 <cchen@bu.edu>
2023-06-09 02:43:00 -0400

    part 1 of having login button direct to login page

--- cs411/src/Home_page.js
+++ cs411/src/Home_page.js
@@ -1,15 +1,23 @@
-
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import './Home_page.css';
 
 const Home_page = () => {
+  const history = useHistory();
+
+  const handleLoginClick = () => {
+    history.push('/login'); // replace '/login' with the path to your login page
+  }
+
   return (
     <div className="home-container">
       <div className="overlay">
         <h1 className="overlay-text">CommuteBeat</h1>
-        <h1 className="body-text">Find the best route to your destination and get a personalized Spotify playlist as you head over! </h1>
-        <button className="login-button">Login</button>
-        </div>
+        <h1 className="body-text">Find the best route to your destination and a personalized Spotify playlist as you head over! </h1>
+      </div>
+      <div className="login-container">
+        <button className="login-button" onClick={handleLoginClick}>Login</button>
+      </div>
     </div>
   );
 };
