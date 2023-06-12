9a56abc9bd417d50f5954c616211b0880b565f6a
picoting <lindsayl.6857@gmail.com>
2023-06-12 10:48:39 -0400

    updating return types

--- directions.py
+++ directions.py
@@ -55,7 +55,7 @@ def validate_address(address, api_key): #returns tuple of formatted address and 
 
     return None, None
 
-#returns dictionary of all modes where the value is tuple (time in text, time in seconds)
+#returns dictionary of all modes where the value is tuple (time in text, time in)
 def route_durations(start, destination, api_key):
     all_durations = {}
     modes = ['driving', 'bicycling', 'transit', 'walking']
@@ -66,11 +66,19 @@ def route_durations(start, destination, api_key):
             for modes, duration in directions_durations.items(): #this displays "transit" as "walking" not sure why
                 if mode != None:
                     all_durations[mode] = duration
-                    print(f"{mode.capitalize()}: {duration[0]}") #prints mode from the list and not the dictionary
-    
     return(all_durations)
 
-print(route_durations(start, destination, api_key))
+def print_durations(durations):
+    for modes, duration in durations.items(): #this displays "transit" as "walking" not sure why
+        if modes != None:
+                print(f"{modes.capitalize()}: {duration[0]}") #prints mode from the list and not the dictionary
+
+
+
+durations = route_durations(start, destination, api_key)
+print_durations(durations)
 #print(get_directions_duration(start, destination, 'transit', api_key))
+
+
 
 
--- recommendations.py
+++ recommendations.py
@@ -1,20 +1,35 @@
 import weather
 import directions
 
-start = ''
-destination = ''
+start = '808 Commonwealth Ave Boston MA'
+destination = '528 Beacon St Boston MA'
+durations = directions.route_durations(start, destination, directions.api_key)
+zip = (directions.validate_address(start, directions.api_key))[1]
 
-def main(start, destination):
+
+def main(start, destination): #returns recommended mode of transportation
     zip = (directions.validate_address(start, directions.api_key))[1]
+    durations = directions.route_durations(start, destination, directions.api_key)
     if zip:
         temp, condition = weather.get_weather(zip, weather.api_key)
-    
-    if condition != ('Sunny' or 'Clear' or 'Partly cloudy' or 'Cloudy' or 'Overcast'):
-        i = 1
+
+        if condition != ('Sunny' or 'Clear' or 'Partly cloudy' or 'Cloudy' or 'Overcast'):
+            if temp > 80:
+                return 'driving'
+            else:
+                return 'walking'
+        else:
+            return shortest_time(durations)
     return None
 
-def shortest_time(durations):
-    temp = min(durations.values())
-    res = [key for key in durations if durations[key] == temp]
+def shortest_time(durations): #returns shortest mode of transportation
+    temp = min([value[1] for value in durations.values()])
+    res = [key for key in durations if durations[key][1] == temp]
+    return res[0]
+
+
+#print(durations)
+
+print(main(start, destination))
 
     
