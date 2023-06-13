0ebd729c219e17a71684b241d25ee0758cfb277c
celinec1 <cchen@bu.edu>
2023-06-13 12:09:27 -0400

    updated

--- cs411/src/App.js
+++ cs411/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home_page from './Home_page';
 import "./App.css";
 import LoginPage from './login'; // import the login component
@@ -9,20 +8,12 @@ import WebPage from './WebPage'
 function App() {
     return (
       <div className="App">
-        {/* <Home_page /> */}
-        <WebPage />
+        <Home_page />
+        {/* <WebPage /> */}
         <header className="App-header"> </header>
       </div>
     );
   }
 
-// const App = () => {
-//   return (
-//     <Router>
-//       <Route path="/" exact component={HomePage} />
-//       <Route path="/login" component={LoginPage} />  // set the path to your login component
-//     </Router>
-//   );
-// };
 
 export default App;
--- cs411/src/Home_page.js
+++ cs411/src/Home_page.js
@@ -24,18 +24,39 @@
 
 // export default Home_page;
 
+// import React from 'react';
+// import './Home_page.css';
+
+// const Home_page = () => {
+//   return (
+//     <div className="home-container">
+//       <div className="overlay">
+//         <h1 className="overlay-text">CommuteBeat</h1>
+//         <h1 className="body-text">Find the best route to your destination and get a personalized Spotify playlist as you head over!</h1>
+//         <button className="login-button">Login</button>
+//       </div>
+//       </div>
+//   );
+// };
+
+// export default Home_page;
+
 import React from 'react';
 import './Home_page.css';
 
 const Home_page = () => {
+  const handleLogin = () => {
+    window.location.href = 'http://localhost:8000/'; // Replace with the URL of your Flask application
+  };
+
   return (
     <div className="home-container">
       <div className="overlay">
         <h1 className="overlay-text">CommuteBeat</h1>
         <h1 className="body-text">Find the best route to your destination and get a personalized Spotify playlist as you head over!</h1>
-        <button className="login-button">Login</button>
+        <button className="login-button" onClick={handleLogin}>Login</button>
       </div>
-      </div>
+    </div>
   );
 };
 
