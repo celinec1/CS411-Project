9eed9541a8711c6a2949dd032efcbe9dca410126
celinec1 <cchen@bu.edu>
2023-06-13 10:20:00 -0400

    updating page

--- cs411/src/WebPage.js
+++ cs411/src/WebPage.js
@@ -6,121 +6,40 @@ const WebPage = () => {
   const [selectedTransportation, setSelectedTransportation] = useState('');
   const [isTransportationSubmitted, setIsTransportationSubmitted] = useState(false);
 
-
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//     const location = event.target.locationInput.value;
-//     const destination = event.target.destinationInput.value;
-
-//     console.log('Location:', location);
-//     console.log('Destination:', destination);
-
-//     event.target.reset();
-//     setIsFormSubmitted(true);
-//   };
-
-//   const handleTransportationSelect = (transportation) => {
-//     setSelectedTransportation(transportation);
-//   };
-
-//   const handlePrevious = () => {
-//     setIsFormSubmitted(false);
-//     setSelectedTransportation('');
-//   };
-
-//   return (
-//     <div>
-//       <div className="navbar">
-//         <h1>CommuteBeat</h1>
-//         <button>Profile</button>
-//       </div>
-
-//       {!isFormSubmitted ? (
-//         <div className="user-input">
-//           <h2>Enter your location and destination:</h2>
-//           <form onSubmit={handleSubmit}>
-//             <input type="text" id="locationInput" placeholder="Enter location" required />
-//             <input type="text" id="destinationInput" placeholder="Enter destination" required />
-//             <button type="submit">Submit</button>
-//           </form>
-//         </div>
-//       ) : (
-//         <div className="transportation-selection">
-//           <h2>Select mode of transportation:</h2>
-//           <div className="transportation-buttons">
-//             <button
-//               className={`transportation-button ${selectedTransportation === 'walk' ? 'selected' : ''}`}
-//               onClick={() => handleTransportationSelect('walk')}
-//             >
-//               Walk
-//             </button>
-//             <button
-//               className={`transportation-button ${selectedTransportation === 'cycle' ? 'selected' : ''}`}
-//               onClick={() => handleTransportationSelect('cycle')}
-//             >
-//               Cycle
-//             </button>
-//             <button
-//               className={`transportation-button ${selectedTransportation === 'car' ? 'selected' : ''}`}
-//               onClick={() => handleTransportationSelect('car')}
-//             >
-//               Car
-//             </button>
-//             <button
-//               className={`transportation-button ${selectedTransportation === 'train' ? 'selected' : ''}`}
-//               onClick={() => handleTransportationSelect('train')}
-//             >
-//               Train
-//             </button>
-//           </div>
-//           <div className="button-group">
-//           <button className="submit-button">Submit</button>
-//             <button className="previous-button" onClick={handlePrevious}>
-//               Previous
-//             </button>
-//           </div>
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default WebPage;
-
-const handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
     const location = event.target.locationInput.value;
     const destination = event.target.destinationInput.value;
-  
+
     console.log('Location:', location);
     console.log('Destination:', destination);
-  
+
     event.target.reset();
     setIsFormSubmitted(true);
   };
-  
+
   const handleTransportationSelect = (transportation) => {
     setSelectedTransportation(transportation);
-    setIsTransportationSubmitted(false); // Reset the transportation submission when a new mode of transportation is selected
+    setIsTransportationSubmitted(false);
   };
-  
+
   const handlePrevious = () => {
     setIsFormSubmitted(false);
     setSelectedTransportation('');
-    setIsTransportationSubmitted(false); // Reset the transportation submission when going back
+    setIsTransportationSubmitted(false);
   };
-  
+
   const handleTransportationSubmit = () => {
     setIsTransportationSubmitted(true);
   };
-  
+
   return (
     <div>
       <div className="navbar">
         <h1>CommuteBeat</h1>
         <button>Profile</button>
       </div>
-  
+
       {!isFormSubmitted ? (
         <div className="user-input">
           <h2>Enter your location and destination:</h2>
@@ -166,13 +85,17 @@ const handleSubmit = (event) => {
             <button className="submit-button" onClick={handleTransportationSubmit}>
               Submit
             </button>
-            {isTransportationSubmitted && (
-              <p>Here is the Spotify playlist!</p>
-            )}
           </div>
+          {isTransportationSubmitted && (
+            <div className="playlist-section">
+              <h2>Here is the Spotify playlist!</h2>
+            </div>
+          )}
         </div>
       )}
     </div>
   );
-            };
-            export default WebPage;
+};
+
+export default WebPage;
+
