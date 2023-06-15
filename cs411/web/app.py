6412f848b993497c2d3eb152b74128a3ad43cc7c
celinec1 <cchen@bu.edu>
2023-06-15 01:20:05 -0400

    to be updated with branch

--- cs411/web/app.py
+++ cs411/web/app.py
@@ -1,33 +1,202 @@
-import json
+# import json
+# import random
+# from flask import Flask, render_template, request, redirect, url_for, jsonify
+# from flask_cors import CORS
+# import requests
+# from urllib.parse import quote
+
+
+# app = Flask(__name__)
+# user = ''
+# access = ''
+
+# #working on taking in inputs 
+# @app.route('/api/submit', methods=['POST'])
+# def submit():
+#     data = request.get_json()
+#     location = data.get('location')
+#     destination = data.get('destination')
+
+#     # Process the location and destination data as needed
+#     # Example: Print the received data
+#     print('Location:', location)
+#     print('Destination:', destination)
+
+#     # Perform any additional actions with the received data
+
+    
+#     return jsonify({'message': 'Data received successfully'})
+
+
+
+# # Step 1: Authorization - Redirect user to Spotify's authorization page
+# @app.route('/')
+# def index():
+#     # Construct the authorization URL
+#     auth_url = 'https://accounts.spotify.com/authorize'
+#     auth_params = {
+#         'client_id': '3a35c0bb12b54e1f8f0602a408bc6bf3',
+#         'response_type': 'code',
+#         'redirect_uri': quote('http://localhost:8000/callback', safe=''),
+#         'scope': 'user-read-private user-read-email user-top-read playlist-modify-private', # Add necessary scopes
+#     }
+
+#     # Redirect the user to the authorization URL
+#     auth_redirect_url = auth_url + '?' + '&'.join([f'{k}={v}' for k, v in auth_params.items()])
+#     return redirect(auth_redirect_url)
+
+
+# def create_top_tracks_playlist(user_id, access_token, num_songs, length):
+#     # create a new playlist
+#     playlist_url = f'https://api.spotify.com/v1/users/{user_id}/playlists'
+#     headers = {
+#         'Authorization': f'Bearer {access_token}',
+#         'Content-Type': 'application/json',
+#     }
+#     playlist_data = {
+#         'name': 'Have a safe trip!',
+#         'public': False,
+#     }
+#     response = requests.post(playlist_url, headers=headers, json=playlist_data)
+
+#     if response.status_code == 201:
+#         playlist_data = response.json()
+#         playlist_id = playlist_data['id']
+#         print('Playlist ID:', playlist_id)
+
+#         # get the user's top tracks
+#         top_tracks_url = 'https://api.spotify.com/v1/me/top/tracks'
+#         params = {
+#             'limit': 200,
+#         }
+#         response = requests.get(top_tracks_url, headers=headers, params=params)
+
+#         if response.status_code == 200:
+#             tracks_data = response.json()
+#             track_ids = [track['id'] for track in tracks_data['items']]
+#             random_track_ids = random.sample(track_ids, num_songs)
+
+#             # add the top tracks to the playlist
+#             add_tracks_url = f'https://api.spotify.com/v1/playlists/{playlist_id}/tracks'
+#             tracks_data = []
+#             for track_id in random_track_ids:
+#                 track_info_url = f'https://api.spotify.com/v1/tracks/{track_id}'
+#                 response = requests.get(track_info_url, headers=headers)
+#                 if response.status_code == 200:
+#                     track_info = response.json()
+#                     tracks_data.append({'uri': f'spotify:track:{track_id}', 'duration_ms': track_info['duration_ms']})
+
+#             # calculate total duration of tracks
+#             total_duration = sum(track['duration_ms'] for track in tracks_data)
+            
+#             # sort tracks by duration
+#             tracks_data.sort(key=lambda x: x['duration_ms'])
+            
+#             # remove tracks to get closest duration to the desired length
+#             while total_duration > length * 1000:
+#                 removed_track = tracks_data.pop()
+#                 total_duration -= removed_track['duration_ms']
+
+#             response = requests.post(add_tracks_url, headers=headers, json={'uris': [track['uri'] for track in tracks_data]})
+
+#             if response.status_code == 201:
+#                 print('Playlist created successfully with top tracks!')
+#                 link = f'https://open.spotify.com/playlist/{playlist_id}'
+#                 print(link)
+#                 return jsonify({link})
+#             else:
+#                 print('Error adding tracks to playlist:', response.text)
+#         else:
+#             print('Error retrieving top tracks:', response.text)
+#     else:
+#         print('Error creating playlist:', response.text)
+
+
+# # Step 2: Obtain authorization code - Handle redirect URI and retrieve the code
+# @app.route('/callback')
+# def callback():
+#     authorization_code = request.args.get('code')
+
+#     # Step 3: Exchange authorization code for access token
+#     token_url = 'https://accounts.spotify.com/api/token'
+#     token_data = {
+#         'client_id': '3a35c0bb12b54e1f8f0602a408bc6bf3',
+#         'client_secret': '17391461037d4d1788e03c089bd4319d',
+#         'grant_type': 'authorization_code',
+#         'code': authorization_code,
+#         'redirect_uri': 'http://localhost:8000/callback',
+#     }
+
+#     response = requests.post(token_url, data=token_data)
+
+#     # Step 4: Use the access token to make API requests
+#     if response.status_code == 200:
+#         token_response = response.json()
+#         access_token = token_response.get('access_token')
+#         refresh_token = token_response.get('refresh_token')
+
+#         if access_token:
+#             # Example API request: Get user profile information
+#             profile_url = 'https://api.spotify.com/v1/me'
+#             headers = {
+#                 'Authorization': f'Bearer {access_token}',
+#             }
+
+#             profile_response = requests.get(profile_url, headers=headers)
+#             profile_data = profile_response.json()
+
+#             if profile_response.status_code == 200:
+#                 profile_data = profile_response.json()
+#                 print('Profile Data:', profile_data)  # Print the entire JSON response
+
+#             # Extract user ID from the response
+#                 user_id = profile_data.get('id')
+#                 display_name = profile_data.get('display_name')
+#                 email = profile_data.get('email')
+
+#                 # Print the user ID and other information
+#                 print('User ID:', user_id)
+#                 print('Display Name:', display_name)
+#                 print('Email:', email)
+
+#                 # Add any additional processing or rendering logic as needed
+#                 create_top_tracks_playlist(user_id, access_token, num_songs, 1200)
+
+#                 # return JSON response
+#                 response_data = {
+#                     'User ID': user_id,
+#                     'Display Name': display_name,
+#                     'Email': email,
+#                 }
+
+#                 return jsonify(response_data)
+            
+#             else:
+#                 error_message = profile_data.get('error', {}).get('message')
+#                 return jsonify({'error': error_message})          
+            
+#     return jsonify({'error': 'Access token not obtained.'})
+
+
+
+# # Usage example
+# access_token = access
+# num_songs = 20  # Number of random songs in the playlist
+
+
+
+# if __name__ == '__main__':
+#     app.run(host='localhost', port=8000, debug=True)
+
+
+
+#testing
 import random
-from flask import Flask, render_template, request, redirect, url_for, jsonify
-from flask_cors import CORS
+from flask import Flask, request, redirect, jsonify
 import requests
 from urllib.parse import quote
 
-
 app = Flask(__name__)
-user = ''
-access = ''
-
-#working on taking in inputs 
-@app.route('/api/submit', methods=['POST'])
-def submit():
-    data = request.get_json()
-    location = data.get('location')
-    destination = data.get('destination')
-
-    # Process the location and destination data as needed
-    # Example: Print the received data
-    print('Location:', location)
-    print('Destination:', destination)
-
-    # Perform any additional actions with the received data
-
-    
-    return jsonify({'message': 'Data received successfully'})
-
-
 
 # Step 1: Authorization - Redirect user to Spotify's authorization page
 @app.route('/')
@@ -38,7 +207,8 @@ def index():
         'client_id': '3a35c0bb12b54e1f8f0602a408bc6bf3',
         'response_type': 'code',
         'redirect_uri': quote('http://localhost:8000/callback', safe=''),
-        'scope': 'user-read-private user-read-email user-top-read playlist-modify-private', # Add necessary scopes
+        'scope': 'user-read-private user-read-email user-top-read playlist-modify-private',
+        # Add necessary scopes
     }
 
     # Redirect the user to the authorization URL
@@ -47,7 +217,7 @@ def index():
 
 
 def create_top_tracks_playlist(user_id, access_token, num_songs, length):
-    # create a new playlist
+    # Create a new playlist
     playlist_url = f'https://api.spotify.com/v1/users/{user_id}/playlists'
     headers = {
         'Authorization': f'Bearer {access_token}',
@@ -64,7 +234,7 @@ def create_top_tracks_playlist(user_id, access_token, num_songs, length):
         playlist_id = playlist_data['id']
         print('Playlist ID:', playlist_id)
 
-        # get the user's top tracks
+        # Get the user's top tracks
         top_tracks_url = 'https://api.spotify.com/v1/me/top/tracks'
         params = {
             'limit': 200,
@@ -76,7 +246,7 @@ def create_top_tracks_playlist(user_id, access_token, num_songs, length):
             track_ids = [track['id'] for track in tracks_data['items']]
             random_track_ids = random.sample(track_ids, num_songs)
 
-            # add the top tracks to the playlist
+            # Add the top tracks to the playlist
             add_tracks_url = f'https://api.spotify.com/v1/playlists/{playlist_id}/tracks'
             tracks_data = []
             for track_id in random_track_ids:
@@ -86,13 +256,13 @@ def create_top_tracks_playlist(user_id, access_token, num_songs, length):
                     track_info = response.json()
                     tracks_data.append({'uri': f'spotify:track:{track_id}', 'duration_ms': track_info['duration_ms']})
 
-            # calculate total duration of tracks
+            # Calculate total duration of tracks
             total_duration = sum(track['duration_ms'] for track in tracks_data)
-            
-            # sort tracks by duration
+
+            # Sort tracks by duration
             tracks_data.sort(key=lambda x: x['duration_ms'])
-            
-            # remove tracks to get closest duration to the desired length
+
+            # Remove tracks to get closest duration to the desired length
             while total_duration > length * 1000:
                 removed_track = tracks_data.pop()
                 total_duration -= removed_track['duration_ms']
@@ -101,9 +271,8 @@ def create_top_tracks_playlist(user_id, access_token, num_songs, length):
 
             if response.status_code == 201:
                 print('Playlist created successfully with top tracks!')
-                link = f'https://open.spotify.com/playlist/{playlist_id}'
-                print(link)
-                return jsonify({link})
+                # Redirect the user to the webpage
+                return redirect('http://localhost:3000/webpage')
             else:
                 print('Error adding tracks to playlist:', response.text)
         else:
@@ -149,7 +318,7 @@ def callback():
                 profile_data = profile_response.json()
                 print('Profile Data:', profile_data)  # Print the entire JSON response
 
-            # Extract user ID from the response
+                # Extract user ID from the response
                 user_id = profile_data.get('id')
                 display_name = profile_data.get('display_name')
                 email = profile_data.get('email')
@@ -162,7 +331,7 @@ def callback():
                 # Add any additional processing or rendering logic as needed
                 create_top_tracks_playlist(user_id, access_token, num_songs, 1200)
 
-                # return JSON response
+                # Return JSON response
                 response_data = {
                     'User ID': user_id,
                     'Display Name': display_name,
@@ -170,26 +339,18 @@ def callback():
                 }
 
                 return jsonify(response_data)
-            
+
             else:
                 error_message = profile_data.get('error', {}).get('message')
-                return jsonify({'error': error_message})          
-            
+                return jsonify({'error': error_message})
+
     return jsonify({'error': 'Access token not obtained.'})
 
 
-
-# Usage example
-access_token = access
+# access_token = access
 num_songs = 20  # Number of random songs in the playlist
-
-
-
 if __name__ == '__main__':
     app.run(host='localhost', port=8000, debug=True)
-
-
-
 
 
 
